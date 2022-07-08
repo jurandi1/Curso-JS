d236130f7083d5fac7eb8a4f4d832405d7060920
Jurandi Junior <jurandijr20@gmail.com>
2022-07-08 18:59:33 -0300

    Analisador de números P2(Finalizado)

--- Aula 16ex/ex018/script.js
+++ Aula 16ex/ex018/script.js
@@ -26,8 +26,39 @@ function inLista(n, l) {
 //-----------------------------------------------------
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement(`option`)
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     }else {
         window.alert(`Valor inválido ou já encontrado na lista.`)
+    }
+    num.value =''
+    num.focus() 
+}
+//________________________________________________________________
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot =  valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]               // A estrutura for com If ira analisar qual o valor menor e maior
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        } 
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A media de todos os valores é ${media}</p>`
     }
 }
